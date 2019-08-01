2496324cc5bdcccb7303c0a675ed718abb7d8f55
Dmitriy Sorokin <da_sorokin@mail.ru>
2019-08-01 10:56:34 +0300

    finished pages - pixel perfect

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -1,8 +1,10 @@
-var header = document.querySelector('.js-header');
-var headerBtn = document.querySelector('.js-header__btn');
-var nav = document.querySelector('.js-nav');
-var listIconsToggle = headerBtn.querySelectorAll('.js-icon_toggle');
-var modOpened = 'is-opened';
+const header = document.querySelector('.js-header');
+const headerBtn = document.querySelector('.js-header__btn');
+const nav = document.querySelector('.js-nav');
+const filterBtns = document.querySelectorAll('.js-filter__btn');
+const listIconsToggle = headerBtn.querySelectorAll('.js-icon_toggle');
+const modOpened = 'is-opened';
+const TABLET = 660;
 
 // if JS(true)
 nav.classList.remove('is-no-js');
@@ -13,8 +15,23 @@ headerBtn.addEventListener('click', function toggleMenu() {
   nav.classList.toggle(modOpened);
   header.classList.toggle(modOpened);
 
-  for (var i = 0; i < listIconsToggle.length; i++) {
-    var el = listIconsToggle[i];
+  for (let i = 0; i < listIconsToggle.length; i++) {
+    const el = listIconsToggle[i];
     el.classList.contains(modOpened) ? el.classList.remove(modOpened) : el.classList.add(modOpened);
   }
 });
+
+// определяем width window
+const w = window;
+const d = document;
+const e = d.documentElement;
+const g = d.getElementsByTagName('body')[0];
+const windowWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+// добавляем attr disabled
+if (windowWidth >= TABLET) {
+  for (let i = 0; i < filterBtns.length; i++) {
+    const el = filterBtns[i];
+    el.setAttribute('disabled', 'disabled');
+  }
+}
